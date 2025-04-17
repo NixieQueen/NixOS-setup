c896528a5a32b71b0453f6721881d94fddbe080e
NixieQueen <thestampyliker@gmail.com>
2025-04-17 14:52:13 +0200

    Fix build issue

--- configurations/overlays/default.nix
+++ configurations/overlays/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  imports = [ ./substituters.nix ./picom-ft-labs.nix ./lua-pam.nix ./lightdm-webkit2.nix ];
+  imports = [ ./substituters.nix ./picom-ft-labs.nix ./lua-pam.nix ];
   
   nixpkgs.overlays = [
     inputs.nixpkgs-f2k.overlays.window-managers
