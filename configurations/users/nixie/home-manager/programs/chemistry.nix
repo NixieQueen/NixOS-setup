9be02055de1dc313340d9c3f411f43a967ac3545
NixieQueen <thestampyliker@gmail.com>
2025-09-13 15:45:45 +0200

    Revert "Setup: lammps-gui"
    This reverts commit 849b257edb27fac4bf8bd4ebc2dc7aebce036cbd.

--- configurations/users/nixie/home-manager/programs/chemistry.nix
+++ configurations/users/nixie/home-manager/programs/chemistry.nix
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 { pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     #lammps
     #iqmol
-=======
-{ pkgs }:
-
-{
-  home.packages = with pkgs; [
-    lammps
->>>>>>> f4fc22d (Setup: lammps-gui)
   ];
 }
